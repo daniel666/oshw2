280f92f66cf48f324170994a48224e9909a376ce
xushunyi@PC <xushunyi@gmail.com>
2013-02-26 14:35:59 -0500

    Fixed the system call completely. Move on to play with set colors

--- kernel/color.c
+++ kernel/color.c
@@ -6,65 +6,89 @@
 #include <linux/sched.h>
 #include <asm/uaccess.h>
 #define debug 1
-void print_int_2d(int *arr, int n, char* message)
+void print_task(struct task_struct* tsk)
+{
+      printk("-------Task------\n");
+      printk("pid: %d\n", tsk->pid);
+      printk("tgid: %d\n",tsk->tgid);
+      printk("-------End------\n");
+}
+void print_pids(int *arr, int n, char* message)
 {
    int i;
    if(message)
-         printk("%s\n", message);
+         printk("%s:", message);
    for(i=0; i<n; i++)   {
       printk("%d ", arr[i]);
    }
+   printk("\n");
 }
 
+void print_colors(u_int16_t *arr, int n, char* message)
+{
+   int i;
+   if(message)
+         printk("%s:", message);
+   for(i=0; i<n; i++)   {
+      printk("%d ", arr[i]);
+   }
+   printk("\n");
+}
 rwlock_t lock = RW_LOCK_UNLOCKED;
 
 SYSCALL_DEFINE4(set_colors, int, nr_pids, pid_t *, pids, u_int16_t *, colors, int *, retval)
 {
       pid_t  kpids[nr_pids];
       u_int16_t kcolors[nr_pids];
-      int   kretval[nr_pids];
+      int   kretval[nr_pids] ;
       int   i;
-      struct task_struct *task;
       struct task_struct *sibling_entry;
       struct task_struct *child_entry;
       struct list_head* sibling;
       struct list_head* child;
-      pid_t  tgid;
 
-      if(debug){
-            printk("IN set_color\n");
-            print_int_2d(pids, nr_pids, "Output pids in kernel");
-            print_int_2d(colors, nr_pids, "Output color info");
-      }
       //if not root, return error
       if(current->real_cred->uid)
             return -EACCES;
-      if(copy_from_user(kpids, pids, sizeof(nr_pids)))
+      if(copy_from_user(kpids, pids, sizeof(pid_t)*nr_pids))
             return -EFAULT;
-      if(copy_from_user(kcolors, colors, sizeof(nr_pids)))
+      if(copy_from_user(kcolors, colors, sizeof(u_int16_t)*nr_pids))
             return -EFAULT;
 
       if(debug){
-            print_int_2d(kpids, nr_pids, "Output kpids in kernel");
-            print_int_2d(kcolors, nr_pids, "Output kcolor info");
+            printk("-----SET_COLORS------\n");
+            print_pids(kpids, nr_pids, "Output kpids:");
+            print_colors(kcolors, nr_pids, "Output kcolor:");
       }
-      printk("HERE\n");
       for(i=0;i<nr_pids;i++){
+            struct task_struct *tmp_task;
+            struct task_struct *task;
+            struct task_struct * thread_task;
+            pid_t  tgid;
+
             //need to lock pid???
-            task = find_task_by_vpid(kpids[i]);
+            tmp_task = find_task_by_vpid(kpids[i]);
             if(!task){
                    kretval[i]=-EINVAL;
+                   printk("Found no task by %d\n", kpids[i]);
                    continue;
             }
-            tgid = task->tgid;
-            struct task_struct * thread_task;
-            while_each_thread(thread_task, task){
-                  unsigned long flags=0;
-                  write_lock_irqsave(&lock, flags);
-                  thread_task->color = kcolors[i];
-                  kretval[i] = 0;
-                  write_unlock_irqrestore(&lock, flags);
-            }
+            tgid =tmp_task->tgid;
+            if(debug)
+                  print_task(tmp_task);
+
+            continue;
+            write_lock_irq(&tasklist_lock);
+            do_each_thread(task, thread_task){
+                  if(thread_task->tgid = tgid){
+                  //unsigned long flags=0;
+                  //write_lock_irqsave(&lock, flags);
+                        thread_task->color = kcolors[i];
+                        kretval[i] = 0;
+                  //write_unlock_irqrestore(&lock, flags);
+                  }
+            }while_each_thread(task, thread_task);
+            write_unlock(&tasklist_lock);
 /*-------------------------------------------------------------------------------------------------------------------------------
 Maybe I don't need this part
 
@@ -93,8 +117,9 @@ Maybe I don't need this part
             }
  ------------------------------------------------------------------------------------------------------------------------------------*/
       }
-      if(copy_to_user(retval,kretval, sizeof(nr_pids)))
+      if(copy_to_user(retval,kretval, sizeof(int)*nr_pids))
             return -EFAULT;
+      print_pids(retval, nr_pids, "retval:");
 }
 
 SYSCALL_DEFINE4(get_colors, int, nr_pids, pid_t *, pids, u_int16_t *, colors, int *, retval)
@@ -110,12 +135,16 @@ SYSCALL_DEFINE4(get_colors, int, nr_pids, pid_t *, pids, u_int16_t *, colors, in
       struct list_head* child;
       pid_t  tgid;
 
-      printk("IN get_color\n");
-      if(copy_from_user(kpids, pids, sizeof(nr_pids)))
-            return -EFAULT;
-      if(copy_from_user(kcolors, colors, sizeof(nr_pids)))
+      printk("-----GET_COLORS------\n");
+
+      if(copy_from_user(kpids, pids, sizeof(pid_t)*nr_pids))
             return -EFAULT;
 
+      if(debug){
+            print_pids(kpids, nr_pids, "Output kpids:");
+      }
+
+      return 0;
       for(i=0; i<nr_pids; i++){
             task = find_task_by_vpid(kpids[i]);
             if(!task){
@@ -128,6 +157,6 @@ SYSCALL_DEFINE4(get_colors, int, nr_pids, pid_t *, pids, u_int16_t *, colors, in
             kretval[i] = 0;
             read_unlock_irqrestore(&lock, flags);
       }
-      if(copy_to_user(retval,kretval, sizeof(nr_pids)))
+      if(copy_to_user(retval,kretval, sizeof(int)*nr_pids))
             return -EFAULT;
 }
--- testFile/test.c
+++ testFile/test.c
@@ -7,14 +7,14 @@
 int main()
 {
    #define nr_pids  5
-   pid_t    pids[nr_pids] = {1 ,2 , 3, 4, 5};
+
+   pid_t    pids[nr_pids] = {22511,22589, 23542, 25043,25219};
    u_int16_t   colors[nr_pids] = {1, 2, 3, 4, 5};
    int retval[nr_pids];
 
    printf("User space call start: ready to call sys %d\n",set_colors);
    syscall(set_colors, nr_pids, pids, colors, retval);
    printf("User space call end\n");
-
 
    u_int16_t   ret_colors[nr_pids];
    int   retval2[nr_pids];
